3c7a80c5af13663abe23cbd8e8a0fa92ffe26dbc
cperrea1 <cperrea1@unb.ca>
2024-08-29 21:53:02 -0300

    first commit

---
+++ Sources/Models/Player.swift
@@ -0,0 +1,34 @@
+//
+//  Player.swift
+//
+//
+//  Created by Courtney Perreault on 2024-08-28.
+//
+
+import Vapor
+import Fluent
+
+public final class Player: Model, Content{
+    
+    public static let schema = "users"
+    
+    
+    @ID(key: .id)
+    public var id: UUID?
+    
+    @Field(key: "first_name")
+    public var firstName: String
+    
+    @Field(key: "last_name")
+    public var lastName: String
+    
+    public init(){
+        
+    }
+    
+    public init(id: UUID? = nil, firstName: String, lastName: String){
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
